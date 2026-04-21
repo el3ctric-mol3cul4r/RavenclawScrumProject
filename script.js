e22cf43d34de9d4a36b0681e71f45f8e792078e0
Ryan Schutte <rmschutte09@gmail.com>
2026-04-21 11:41:14 -0400

    temp commit

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+class Question{
+    constructor(answer){
+        this.answer = answer
+        
+    }
+    
+}
 
+class Subject{
+    constructor(questions, type){
+        this.questions = questions
+        this.type = type
+    }
+
+    newQuestion(){
+        this.display(this.questions[Math.trunc(Math.random()*this.questions.length)])
+    }
+
+    display(question){
+
+    }
+
+}
